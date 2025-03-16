14795ff0fc803c736989ed41716fa7731732d87f
Egor <ep9821123@gmail.com>
2025-03-16 20:01:27 +0300

    feat: set config

---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	User        string        `yaml:"user" env-required:"false"`
+	Password    string        `yaml:"password" env-required:"false" env:"HTTP_SERVER_PASSWORD"`
+}
+
+// MustLoad загружает конфигурацию из конфиг-файла и возвращает ее экземпляр
+func MustLoad() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// Проверяем существование конфиг-файла
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	// Символ ; в данном коде используется для разделения операторов в одной строке. Он позволяет объединить объявление переменной err и условие if в одну конструкцию
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
