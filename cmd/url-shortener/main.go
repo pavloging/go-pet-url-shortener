4f1dd8a15e45cc8a4658a428b5928471bbe5282c
Egor <ep9821123@gmail.com>
2025-03-21 21:37:05 +0300

    feat: created and set new logger with colors

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,8 +4,14 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/handlers/slogpretty"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	mwLogger "url-shortener/internal/http-server/middleware/logger"
 )
 
 const (
@@ -20,6 +26,7 @@ func main() {
 
 	log.Info("starting the project...", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+	log.Error("error messages are enabled")
 
 	storage, err := sqlite.New(cfg.StoragePath)
 	if err != nil {
@@ -27,13 +34,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	// alias, _ := storage.GetUrl("google")
-	// fmt.Println(alias)
+	router := chi.NewRouter()
 
-	storage.SaveURL("https://vk.com", "vk")
+	router.Use(middleware.RequestID) // Хороший middleware для логирования
+	router.Use(mwLogger.New(log))    // Хороший middleware для логирования (custom)
 
-	count, _ := storage.DeleteURL("vk")
-	log.Info("item deleted: ", slog.Int64("count", count))
+	router.Use(middleware.Logger)    // Логирует все входящие запросы
+	router.Use(middleware.Recoverer) // Перехватывает паники и возвращает 500
+	router.Use(middleware.URLFormat) // Для красивых URL при подключении к обработчикам
 
 	_ = storage
 
@@ -47,10 +55,22 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
 	case envProd:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
